61eed4474d48bbd295db506e5d423f808d5d974f
ziyangwang <794140708@qq.com>
2024-12-04 19:17:17 +0800

    fix: 【低】点击幻灯片需要复制对应的网页，并用toast提示。

--- src/components/swiper/index.tsx
+++ src/components/swiper/index.tsx
@@ -3,8 +3,17 @@ import { Autoplay } from 'swiper/modules';
 import swiper1 from '../../assets/swiper/1.png';
 import swiper2 from '../../assets/swiper/2.png';
 import 'swiper/css';
+import copy from 'copy-to-clipboard';
+import { Toast } from 'antd-mobile';
 
 export const HomeSwiper = () => {
+  const handleCopyLink = (link: string) => {
+    copy(link);
+    Toast.show({
+      content: '复制成功'
+    });
+  };
+  
   return (
     <Swiper
       modules={[Autoplay]}
@@ -15,10 +24,10 @@ export const HomeSwiper = () => {
       slidesOffsetAfter={20}
     >
       <SwiperSlide>
-        <img className="rounded-2xl" src={swiper1} alt="" />
+        <img className="rounded-2xl" src={swiper1} alt="" onClick={() => handleCopyLink("https://baidu.com")} />
       </SwiperSlide>
       <SwiperSlide>
-        <img className="rounded-2xl" src={swiper2} alt="" />
+        <img className="rounded-2xl" src={swiper2} alt="" onClick={() => handleCopyLink("https://baidu.com")} />
       </SwiperSlide>
     </Swiper>
   );
