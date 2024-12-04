9ed46b61462119557645ea6075b26f55ed8bbede
ziyangwang <794140708@qq.com>
2024-12-04 19:01:13 +0800

    fix: 【高】用户没有注册，可以直接进入 home 或者其他页面。应该要跳转到邀请码页面

--- src/components/layout/login.tsx
+++ src/components/layout/login.tsx
@@ -1,14 +1,30 @@
 import { useEffect } from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
 import { useAccount } from 'wagmi';
+import { getUserNoToast } from '../../utils/api';
+import { Toast } from 'antd-mobile';
 
 export const LoginLayout = () => {
-  const { isConnected } = useAccount();
+  const { isConnected, address } = useAccount();
   const navigate = useNavigate();
 
   useEffect(() => {
     if (!isConnected) {
       navigate('/');
+    } else {
+      getUserNoToast({ address: address as string }).then((res) => {
+        if (!res.data.data) {
+          Toast.show({
+            content: '请先连接钱包',
+          });
+          navigate('/');
+        }
+      }).catch(() => {
+        Toast.show({
+          content: '请先连接钱包',
+        });
+        navigate('/');
+      });
     }
   }, [isConnected]);
 
