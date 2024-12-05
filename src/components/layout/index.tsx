c13fa8e668196f89036eebfbd482ca3e04f35113
ziyangwang <794140708@qq.com>
2024-12-05 14:58:25 +0800

    fix: 【高】在首页，将页面拉到底部。点击收益，收益感觉也拉到了底部。切换页面，应该是拉到顶部的感觉。

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,7 +1,14 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import { Footer } from './footer';
+import { useEffect } from 'react';
 
 export const IndexLayout = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+  
   return (
     <>
       <div className="pb-12">
