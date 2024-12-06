2ffcf9012cf75f01f3ec640f78b438c9e424ecf5
Luke <luchenqun@qq.com>
2024-12-06 22:26:16 +0800

    add sign message

--- src/components/layout/login.tsx
+++ src/components/layout/login.tsx
@@ -1,30 +1,41 @@
 import { useEffect } from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
-import { useAccount } from 'wagmi';
+import { useAccount, useSignMessage } from 'wagmi';
 import { getUserNoToast } from '../../utils/api';
 import { Toast } from 'antd-mobile';
 
 export const LoginLayout = () => {
   const { isConnected, address } = useAccount();
+  const { signMessageAsync } = useSignMessage();
   const navigate = useNavigate();
 
   useEffect(() => {
     if (!isConnected) {
       navigate('/');
     } else {
-      getUserNoToast({ address: address as string }).then((res) => {
-        if (!res.data.data) {
+      signMessageAsync({ message: "Verify your account" }).then(() => {
+        getUserNoToast({ address: address as string })
+        .then((res) => {
+          if (!res.data.data) {
+            Toast.show({
+              content: '请先连接钱包'
+            });
+            navigate('/');
+          }
+        })
+        .catch(() => {
           Toast.show({
-            content: '请先连接钱包',
+            content: '请先连接钱包'
           });
           navigate('/');
-        }
+        });
       }).catch(() => {
         Toast.show({
-          content: '请先连接钱包',
+          content: '请先连接钱包'
         });
         navigate('/');
-      });
+      })
+
     }
   }, [isConnected, address, navigate]);
 
