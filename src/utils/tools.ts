5717f166bd87cbe67c7edbe13f6af1546dfc5b2c
Luke <luchenqun@qq.com>
2024-12-13 17:08:56 +0800

    update for mainnet

--- src/utils/tools.ts
+++ src/utils/tools.ts
@@ -15,14 +15,19 @@ export function usdtToMud(usdt: number | bigint | string, mud_price: number | bi
 }
 
 export const humanReadable = (value: number | bigint | string, precision = 1000000000000000000n) => {
-  // console.log('====================>', value);
-
   value = BigInt(value);
   const result = ((value * 100n) / precision).toString(); // 将结果扩大100倍以保留两位小数
   const integerPart = result.slice(0, -2) || '0'; // 获取整数部分
   const decimalPart = result.slice(-2); // 获取小数部分
 
-  return decimalPart === '00' ? integerPart : `${integerPart}.${decimalPart}`;
+  // 如果小数部分以0结尾,则去掉末尾的0
+  if (decimalPart === '00') {
+    return integerPart;
+  } else if (decimalPart.endsWith('0')) {
+    return `${integerPart}.${decimalPart[0]}`;
+  } else {
+    return `${integerPart}.${decimalPart}`;
+  }
 };
 
 export function formatAddressString(address: string) {
